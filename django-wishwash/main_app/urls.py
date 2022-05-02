6b88a7a0b180af44a2272adf037280e6573e958f
nununuam <nunusnuam@gmail.com>
2022-05-02 14:27:59 -0700

    added the routes and connec them to the templates

--- django-wishwash/main_app/urls.py
+++ django-wishwash/main_app/urls.py
@@ -1,6 +1,33 @@
-from django.urls import path
-from . import views
+from django.shortcuts import render
+from django.views import View 
+from django.http import HttpResponse 
+from django.views.generic.base import TemplateView
+from .models import Task, Categories
+from django.views import View
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect 
+from django.views.generic.base import TemplateView
+from django.contrib.auth.models import User
+from django.views.generic import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 urlpatterns = [
     path('', views.Home.as_view(), name="home"), 
+    path('movies/', views.Movies.as_view(), name="movies"), 
+    path('books/', views.Books.as_view(), name="books"),
+    path('broadways/', views.Broadways.as_view(), name="broadways"),
+    path('createList/', views.CreateList.as_view(), name="createList"),
+    path('edit&deleteList/<int:pk>', views.EditDeleteList.as_view(), name="editDeleteList"),
+    path('user/<username>', views.Profile, name="profile"),
+    path('lists/<int:pk>', views.DetailListPage.as_view(), name="home"), 
+    path('books/<int:pk>', views.DetailBookPage.as_view(), name="detailBook"),
+    path('broadways/<int:pk>', views.DetailBroadwayPage.as_view(), name="detailBroadway"),
+    path('movies/<int:pk>', views.DetailMoviePage.as_view(), name="detailMovie"),
+    path('accounts/signup/', views.Signup, name='signup'),
 ]
--- django-wishwash/main_app/views.py
+++ django-wishwash/main_app/views.py
@@ -6,3 +6,36 @@ from django.views.generic.base import TemplateView
 
 class Home(TemplateView):
     template_name = "home.html"
+
+class Movies(TemplateView):
+    template_name = "movies.html"
+
+class Books(TemplateView):
+    template_name = "books.html"
+
+class Broadways(TemplateView):
+    template_name = "broadways.html"
+
+class CreateList(TemplateView):
+    template_name = "createList.html"
+
+class EditDeleteList(TemplateView):
+    template_name = "editDeleteList.html"
+
+def Profile(request, username):
+    template_name = "profile.html"
+
+class DetailListPage(TemplateView):
+    template_name = "detailList.html"
+
+class DetailBookPage(TemplateView):
+    template_name = "detailBook.html"
+
+class DetailBroadwayPage(TemplateView):
+    template_name = "detailBroadway.html"
+
+class DetailMoviePage(TemplateView):
+    template_name = "detailMovie.html"
+
+def Signup(request):
+    template_name = "signup.html"
