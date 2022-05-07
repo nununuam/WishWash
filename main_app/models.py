f43bc5f08d9fc3ffe81f767f6d07af45b4db0193
janiceh94 <haesung94@gmail.com>
2022-05-07 11:10:06 -0700

    got create pages to work and show on details page

--- main_app/models.py
+++ main_app/models.py
@@ -7,28 +7,11 @@ class List(models.Model):
     def __str__(self):
         return self.name
 
-class Book(models.Model):
-    title = models.CharField(max_length=250)
-    img = models.CharField(max_length=1000)
-    author = models.CharField(max_length=250)
-    genre = models.CharField(max_length=250)
-    preview = models.CharField(max_length=1000)
-    list = models.ManyToManyField(List)
-
-    def __str__(self):
-        return self.title
-    
-    class Meta:
-        ordering = ['title']
-
 class Movie(models.Model):
     title = models.CharField(max_length=250)
     img = models.CharField(max_length=1000)
     cast = models.CharField(max_length=1000)
     trailer = models.CharField(max_length=1000)
-    book = models.CharField(max_length=1000)
-    plays = models.CharField(max_length=1000)
-    list = models.ManyToManyField(List)
 
     def __str__(self):
         return self.title
@@ -41,12 +24,28 @@ class Play(models.Model):
     img = models.CharField(max_length=1000)
     director = models.CharField(max_length=250)
     cast = models.CharField(max_length=1000)
-    book = models.CharField(max_length=1000)
-    movie = models.CharField(max_length=1000)
-    list =  models.ManyToManyField(List)
 
     def __str__(self):
         return self.title
 
     class Meta:
         ordering = ['title']
+
+class Book(models.Model):
+    title = models.CharField(max_length=250)
+    img = models.CharField(max_length=1000)
+    author = models.CharField(max_length=250)
+    genre = models.CharField(max_length=250)
+    preview = models.CharField(max_length=1000)
+    movies = models.ManyToManyField(Movie)
+    plays = models.ManyToManyField(Play)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
+
+
+
--- main_app/urls.py
+++ main_app/urls.py
@@ -6,31 +6,28 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.Home.as_view(), name="home"), 
 
-    path('movies/', views.Movies.as_view(), name="movies"), 
+    path('movies/', views.Movies.as_view(), name="movies"),
+    path('movies/new/', views.AddMovie.as_view(), name='addmovie'), 
+    path('movies/<int:pk>', views.DetailMoviePage.as_view(), name="detailMovie"),
+    path('addmovie/', views.AddMovie.as_view(), name="addmovie"),
+    path('moive/<int:pk>/delete', views.MovieDelete.as_view(), name="moviedelete"),
+    path('moive/<int:pk>/edit', views.MovieEdit.as_view(), name="movieedit"),
+    
     path('books/', views.Books.as_view(), name="books"),
-    path('broadways/', views.Broadways.as_view(), name="broadways"),
-
     path('books/new/', views.AddBook.as_view(), name='addbook'),
-    path('movies/new/', views.AddMovie.as_view(), name='addmovie'),
-    path('broadways/new/', views.AddPlay.as_view(), name='addplay'),
+    path('books/<int:pk>', views.DetailBookPage.as_view(), name="detailBook"),
+    path('addbook/', views.AddBook.as_view(), name="addbook"),
+    path('book/<int:pk>/delete', views.BookDelete.as_view(), name="bookdelete"),
+    path('book/<int:pk>/edit', views.BookEdit.as_view(), name="bookedit"),
 
+    path('broadways/', views.Broadways.as_view(), name="broadways"),
+    path('broadways/new/', views.AddPlay.as_view(), name='addplay'),
+    path('broadways/<int:pk>', views.DetailBroadwayPage.as_view(), name="detailBroadway"),
+    path('addplay/', views.AddPlay.as_view(), name="addplay"),
+    path('broadway/<int:pk>/delete', views.BroadwayDelete.as_view(), name="broadwaydelete"),
+    path('broadway/<int:pk>/edit', views.BroadwayEdit.as_view(), name="broadwayedit"),
 
     path('lists/<int:pk>', views.DetailListPage.as_view(), name="home"), 
-    path('books/<int:pk>', views.DetailBookPage.as_view(), name="detailBook"),
-    path('broadways/<int:pk>', views.DetailBroadwayPage.as_view(), name="detailBroadway"),
-    path('movies/<int:pk>', views.DetailMoviePage.as_view(), name="detailMovie"),
-    
-    path('addbook/', views.AddBook.as_view(), name="addbook"),
-    path('addmovie/', views.AddMovie.as_view(), name="addmovie"),
-    path('addplay/', views.AddPlay.as_view(), name="addplay"),
-
-    path('book/<int:pk>/delete', views.BookDelete.as_view(), name="bookdelete"),
-    path('moive/<int:pk>/delete', views.MovieDelete.as_view(), name="moviedelete"),
-    path('broadway/<int:pk>/delete', views.BroadwayDelete.as_view(), name="broadwaydelete"),
-
-    path('book/<int:pk>/edit', views.BookEdit.as_view(), name="bookedit"),
-    path('moive/<int:pk>/edit', views.MovieEdit.as_view(), name="movieedit"),
-    path('broadway/<int:pk>/edit', views.BroadwayEdit.as_view(), name="broadwayedit"),
 
     path('user/<username>', views.Profile, name="profile"),
     path('accounts/signup/', views.Signup, name='signup'),
